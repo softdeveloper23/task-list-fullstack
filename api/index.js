5b30b1d1ff5f4a49dfae73ba9bd3e2af4e426523
softdeveloper23 <brannongarrett77@gmail.com>
2024-02-05 14:45:50 +0100

    API coding is complete

--- api/index.js
+++ api/index.js
@@ -1,29 +1,71 @@
-const express = require('express')
+import express from 'express';
+import { updateTasks, fetchTasks, createTasks, deleteTasks } from './task';
+import cors from 'cors';
+import serverless from 'serverless-http';
+
 const app = express();
 const port = 3001
 
 app.use(express.json());
+
+if (process.env.DEVELOPMENT) {
+    app.use(cors());
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
 app.get('/task', async (req, res) => {
-    res.send('Hello World!')
+    try {
+        const tasks = await fetchTasks();
+
+        res.send(tasks.Items)
+    } catch (err) {
+        res.status(400).send(`Error fetching tasks: ${err}`)
+    }
 });
 
 app.post('/task', async (req, res) => {
-    res.send('Hello World!')
+    try {
+        const task = req.body;
+
+        const response = await createTasks(task);
+
+        res.send(response);
+    } catch (err) {
+        res.status(400).send(`Error creating tasks: ${err}`)
+    }
 });
 
 app.put('/task', async (req, res) => {
-    res.send('Hello World!')
+    try {
+        const task = req.body;
+
+        const response = await updateTasks(task);
+
+        res.send(response);
+    } catch (err) {
+        res.status(400).send(`Error updating tasks: ${err}`)
+    }
 });
 
-app.delete('/task', async (req, res) => {
-    res.send('Hello World!')
+app.delete('/task/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const response = await deleteTasks(id);
+
+        res.send(response);
+    } catch (err) {
+        res.status(400).send(`Error deleting tasks: ${err}`)
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-});
+if (process.env.DEVELOPMENT) {
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    });
+}
+
+export const handler = serverless(app);
