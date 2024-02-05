5633d0c55407d7c5332bb56d2c691c2c17364c4c
softdeveloper23 <brannongarrett77@gmail.com>
2024-02-05 15:00:41 +0100

    Updated the code for app.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,7 +1,10 @@
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { AddTaskForm } from "./components/AddTaskForm";
 import { Task } from "./components/Task";
+import axios from 'axios';
+import { API_URL } from './utils';
 
 const darkTheme = createTheme({
   palette: {
@@ -9,18 +12,30 @@ const darkTheme = createTheme({
   },
 });
 
-const task = {
-  id: 1,
-  name: 'My first task',
-  completed: false,
-};
-
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get(API_URL);
+
+      setTasks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <AddTaskForm />
-      <Task task={task}/>
+      <AddTaskForm fetchTasks={fetchTasks} />
+      {tasks.map((task) => (
+        <Task task={task} key={task.id} fetchTasks={fetchTasks} />
+      ))}
     </ThemeProvider>
   );
 }
