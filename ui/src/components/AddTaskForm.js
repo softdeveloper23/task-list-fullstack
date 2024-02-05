e973321d209ee6b80831c79e4c72214fd885f697
softdeveloper23 <brannongarrett77@gmail.com>
2024-02-05 11:43:18 +0100

    Added a textfield

---
+++ ui/src/components/AddTaskForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+
+export const AddTaskForm = () => {
+    return (
+        <div>
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </div>
+    )
+}
