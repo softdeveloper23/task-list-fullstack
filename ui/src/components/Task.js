4bd0080a1a7baeef7631e059ff7f763a4daad955
softdeveloper23 <brannongarrett77@gmail.com>
2024-02-05 13:39:06 +0100

    Added code to task file

--- ui/src/components/Task.js
+++ ui/src/components/Task.js
@@ -2,6 +2,8 @@ import { Button, Checkbox, Typography } from '@mui/material';
 import React, { useState } from 'react'
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import { UpdateTaskForm } from './UpdateTaskForm';
+import classnames from 'classnames';
 
 export const Task = ({ task }) => {
     const { id, name, completed } = task;
@@ -17,15 +19,27 @@ export const Task = ({ task }) => {
     }
 
     return (
-    <div>
-        <Checkbox checked={isCompleted} onChange={handleUpdateTaskCompletion} />
-        <Typography variant='h4'>{name}</Typography>
-        <Button variant='contained' onClick={() => setIsDialogOpen(true)}>
-            <EditIcon />
-        </Button>
-        <Button color='error' variant='contained' onClick={handleDeleteTask}>
-            <DeleteIcon />
-        </Button>
+    <div className='task'>
+        <div className={classnames('flex', {
+            done: isCompleted,
+        })}>
+            <Checkbox checked={isCompleted} onChange={handleUpdateTaskCompletion} />
+            <Typography variant='h4'>{name}</Typography> 
+        </div>
+        <div className='taskButtons'>
+            <Button variant='contained' onClick={() => setIsDialogOpen(true)}>
+                <EditIcon />
+            </Button>
+            <Button color='error' variant='contained' onClick={handleDeleteTask}>
+                <DeleteIcon />
+            </Button>
+        </div>
+        
+        <UpdateTaskForm 
+            isDialogOpen={isDialogOpen} 
+            setIsDialogOpen={setIsDialogOpen} 
+            task={task} 
+        />
     </div>
     )
 }
