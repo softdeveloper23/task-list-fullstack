fe2ef88e49f2faee2dd3e469f61899aa07f5246c
softdeveloper23 <brannongarrett77@gmail.com>
2024-02-05 13:56:49 +0100

    Interface is working

--- ui/src/components/UpdateTaskForm.js
+++ ui/src/components/UpdateTaskForm.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Dialog, Button, DialogTitle, TextField } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
 
-export const UpdateTaskForm = () => {
-  return (
-    <div>UpdateTaskForm</div>
-  )
-}
+export const UpdateTaskForm = ({ isDialogOpen, setIsDialogOpen, task }) => {
+    const { id, completed } = task;
+    const { taskName, setTaskName } = useState('');
+    
+    return (
+    <Dialog open={isDialogOpen}>
+            <DialogTitle>Edit Task</DialogTitle>
+            <div className='dialog'>
+                <TextField 
+                    size='small' 
+                    label="Task" 
+                    variant="outlined" 
+                    value={taskName} 
+                    onChange={(e) => setTaskName(e.target.value)}
+                />
+                <Button
+                    variant='contained'
+                    onClick={() => {
+                        setIsDialogOpen(false);
+                    }}
+                >
+                    <CheckIcon />
+                </Button>
+            </div>
+        </Dialog> 
+    );
+};
