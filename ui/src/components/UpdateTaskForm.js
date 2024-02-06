78450fc33010d24e324e7b770a3402532f2e1d49
softdeveloper23 <brannongarrett77@gmail.com>
2024-02-06 12:31:48 +0100

    Updated code to fix a bug

--- ui/src/components/UpdateTaskForm.js
+++ ui/src/components/UpdateTaskForm.js
@@ -1,51 +1,55 @@
-import React, { useState } from 'react'
-import { Dialog, Button, DialogTitle, TextField } from '@mui/material';
-import CheckIcon from '@mui/icons-material/Check';
-import axios from 'axios';
-import { API_URL } from '../utils';
+import React, { useState } from "react";
+import { Button, Dialog, DialogTitle, TextField } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import axios from "axios";
+import { API_URL } from "../utils";
 
-export const UpdateTaskForm = ({ fetchTasks,isDialogOpen, setIsDialogOpen, task }) => {
+export const UpdateTaskForm = ({
+    fetchTasks,
+    isDialogOpen,
+    setIsDialogOpen,
+    task,
+}) => {
     const { id, completed } = task;
-    const { taskName, setTaskName } = useState('');
+    const [taskName, setTaskName] = useState("");
 
     const handleUpdateTaskName = async () => {
-        try {
-            await axios.put(API_URL, {
-                id,
-                name: taskName,
-                completed,
-            });
+    try {
+        await axios.put(API_URL, {
+        id,
+        name: taskName,
+        completed,
+        });
 
-            await fetchTasks();
+        await fetchTasks();
 
-            setTaskName('');
-        } catch (err) {
-            console.log(err);
-        }
-    };
-    
+        setTaskName("");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
     return (
     <Dialog open={isDialogOpen}>
-            <DialogTitle>Edit Task</DialogTitle>
-            <div className='dialog'>
-                <TextField 
-                    size='small' 
-                    label="Task" 
-                    variant="outlined" 
-                    value={taskName} 
-                    onChange={(e) => setTaskName(e.target.value)}
-                />
-                <Button
-                    variant='contained'
-                    onClick={async () => {
-                        await handleUpdateTaskName();
-                        
-                        setIsDialogOpen(false);
-                    }}
-                >
-                    <CheckIcon />
-                </Button>
-            </div>
-        </Dialog> 
+        <DialogTitle>Edit Task</DialogTitle>
+        <div className="dialog">
+        <TextField
+            size="small"
+            label="Task"
+            variant="outlined"
+            onChange={(e) => setTaskName(e.target.value)}
+        />
+        <Button
+            variant="contained"
+            onClick={async () => {
+            await handleUpdateTaskName();
+            
+            setIsDialogOpen(false);
+            }}
+        >
+            <CheckIcon />
+        </Button>
+        </div>
+    </Dialog>
     );
 };
